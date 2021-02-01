e6d00b217be91a00b756478eaf937dd4a7e7b9c6
Cody Green <cody@codygreen.com>
2021-02-01 12:25:49 -0600

    add group lookup

--- cli.py
+++ cli.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 import click
+import sys
+import json
+
+from ms_graph import getGroupId, getGroupMembers
+from msal_interactive_flow import retrieveAccessToken
+
+# Get authorization token
+config = json.load(open("config.json"))
+authorization_token = retrieveAccessToken(
+    config['client_id'], config['authority'], config['scope'])
+
+# Defile CLI group
 
 
 @click.group()
@@ -7,6 +19,7 @@ def cli():
     pass
 
 
+# Add user/group
 @click.command()
 @click.argument('name')
 @click.option('--tenant', prompt='tenant', help='Volterra tenant name is required.')
@@ -19,11 +32,31 @@ def add(name, tenant, createns):
             - user's email address
 
             - Azure AD group name
+
+    createns defaults to False.
     """
     click.echo('adding user/group')
     click.echo(f'tenant:{tenant}')
     click.echo(f'name:{name}')
     click.echo(f'create namespace:{createns}')
+
+    # determine if name is a user or group
+    if "@" in name:
+        click.echo('adding a user')
+        click.echo('FEATURE NOT IMPLEMENTED YET')
+        # Get user data from Azure AD
+        pass
+    else:
+        click.echo('adding a group')
+        # Get group member data from Azure AD
+        try:
+            id = getGroupId(authorization_token, name)
+            users = getGroupMembers(authorization_token, id)
+            print(users)
+        except ValueError as e:
+            click.echo(e)
+
+# remove user/group
 
 
 @click.command()
@@ -38,11 +71,15 @@ def remove(name, tenant, removens):
             - user's email address
 
             - Azure AD group name
+
+    removens defaults to True.
     """
     click.echo('removing user/group')
     click.echo(f'tenant:{tenant}')
     click.echo(f'name:{name}')
     click.echo(f'remove namespace:{removens}')
+    click.echo('FEATURE NOT IMPLEMENTED YET')
+    pass
 
 
 cli.add_command(add)
--- ms_graph.py
+++ ms_graph.py
@@ -2,22 +2,33 @@ import requests
 import json
 from string import Template
 
-graph_url_template = "https://graph.microsoft.com/v1.0/groups/$group_id/transitiveMembers"
+get_group_by_name_url_template = "https://graph.microsoft.com/v1.0/groups?$$filter=displayName eq '$name'&$$select=id"
+get_group_members_url_template = "https://graph.microsoft.com/v1.0/groups/$group_id/transitiveMembers"
 
 
-def getGroupMembers(authorization_token, group_id):
+def getGroupId(authorization_token: str, name: str) -> str:
+    """Obtain Azure AD Group ID based upon Group displayName value"""
+    graph_url = Template(get_group_by_name_url_template).substitute(name=name)
+    resp = getAzureGraph(authorization_token, graph_url)
 
-    # To-do: handle result pagination
+    # ensure we have an ID
+    if resp.json()['value'] and 'id' in resp.json()['value'][0].keys():
+        return resp.json()['value'][0]['id']
+    else:
+        raise ValueError(f'Azure AD Group {name} not found')
 
-    graph_url = Template(graph_url_template)
-    graph_response = requests.get(  # Use token to call downstream service
-        graph_url.substitute(group_id=group_id),
-        headers={'Authorization': 'Bearer ' + authorization_token},)
-    graph_response.raise_for_status()
 
+def getGroupMembers(authorization_token: str, group_id: str) -> str:
+    """Get list of Users belonging to the Azure AD group ID"""
+    # TODO: handle result pagination
+
+    graph_url = Template(get_group_members_url_template).substitute(
+        group_id=group_id)
+
+    resp = getAzureGraph(authorization_token, graph_url)
     users = []
     # Make sure we're processing a user
-    for user in graph_response.json()['value']:
+    for user in resp.json()['value']:
         if user['@odata.type'] == "#microsoft.graph.user":
             users.append({
                 "userPrincipalName": user['userPrincipalName'],
@@ -25,3 +36,11 @@ def getGroupMembers(authorization_token, group_id):
                 "surname": user['surname']
             })
     return users
+
+
+def getAzureGraph(authorization_token: str, url: str) -> str:
+    graph_response = requests.get(  # Use token to call downstream service
+        url,
+        headers={'Authorization': 'Bearer ' + authorization_token},)
+    graph_response.raise_for_status()
+    return graph_response
